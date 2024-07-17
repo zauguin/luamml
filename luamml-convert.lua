413ec0112a504a28a2e9e0d487fa091cb9fa7093
Marcel Fabian Krüger <tex@2krueger.de>
2024-07-17 19:36:49 +0200

    Stop emitting intent="@ignore"

--- luamml-convert.lua
+++ luamml-convert.lua
@@ -616,9 +616,10 @@ function nodes_to_table(head, cur_style, text_families)
     end
     joining = new_joining
   end
-  -- In TeX, groups are never space like
+  -- In TeX, groups are never space like, so we insert an artificial node instead.
+  -- This node should be ignored for most purposes
   if core == space_like then
-    core = {[0] = 'mi', intent = '@ignore'}
+    core = {[0] = 'mi', ['tex:ignore'] = 'true'}
     result[#result+1] = core
   end
   if t[0] == 'mrow' and #t == 1 then
